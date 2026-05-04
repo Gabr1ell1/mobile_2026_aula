39db3315871c984831739ceba356bb6e01b77afb
Gabr1ell1 <gabriellynasser16@gmail.com>
2026-05-04 19:18:53 -0300

    add things

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -3,14 +3,35 @@ import { useState } from 'react';
 
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from '@/components/button';
+import { List } from '@/components/list';
+
+import { useAuth } from '@/context/AuthContext';
 
 export default function Dashboard() {
-     const { username } = useLocalSearchParams<{ username: string }>();
+    const { user, signOut } = useAuth();
+
+    const posts = [
+        { id: '1', title: 'Card 1', description: 'Texto de aviso 1' },
+        { id: '2', title: 'Card 2', description: 'Texto de aviso 2' },
+        { id: '3', title: 'Card 3', description: 'Texto de aviso 3' },
+    ];
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Bem-vindo, {username}!</Text>
-            <Button title="Voltar" onPress={() => router.back()} />
+            <Text style={styles.title}>Bem-vindo, {user}</Text>
+            <Button title="Sair da APP" onPress={signOut} />
+
+            <List
+                data={posts}
+                onLoadMore={() => {}}
+                renderItemContent={(item) => (
+                    <View>
+                        <Text style={styles.cardTitle}>{item.title}</Text>
+                        <Text style={styles.cardText}>{item.description}</Text>
+                    </View>
+                )}
+            />
+
         </View>
     )
 }
@@ -26,7 +47,15 @@ export const styles = StyleSheet.create({
         color: '#333',
         fontSize: 18,
         fontWeight: 'bold',
-        textAlign: 'center',
-        margin: '200px',
-    },      
+    },
+    cardTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    cardText: {
+        fontSize: 14,
+        color: '#666',
+        marginTop: 4,
+    }
 });
